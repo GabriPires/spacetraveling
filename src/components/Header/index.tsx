ce6696c934d754cf54957a6db3feacd1f0cd50bb
Gabriel Pires <gabpiresdev@gmail.com>
2022-06-01 19:37:18 -0300

    feat: header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,23 @@
-export default function Header() {
-  // TODO
-}
+import Image from 'next/image';
+import NextLink from 'next/link';
+
+import styles from './header.module.scss';
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <NextLink href="/">
+        <a>
+          <Image
+            src="/images/logo.svg"
+            alt="logo"
+            width="238px"
+            height="25px"
+          />
+        </a>
+      </NextLink>
+    </header>
+  );
+};
+
+export default Header;
